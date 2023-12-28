b05a58a811a7179d3ccc986f6096f34df8eb0d1f
Zikfer <105168348+Zikfer@users.noreply.github.com>
2023-12-29 02:40:11 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,13 @@
+package config
 
+const BotToken string = "BotToken"
+
+const DbPassword string = "DbPassword"
+
+const DbHost string = "DbHost"
+
+const DbPort int = DbPort
+
+const DbUser string = "DbUser"
+
+const DbName string = "DbName"
